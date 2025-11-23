1626029cc36fc6f65d404e277b70b17a9b3b3ae0
Yuno Kim <unokim1547@gmail.com>
2025-11-23 18:49:42 +0900

    feat(frontend): 로그인 API연결

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,7 @@ function showPage(pageId) {
   document.getElementById(`${pageId}-page`).style.display = 'block';
 
   // header 메뉴 표시
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('accessToken');
   if(token){
     document.getElementById('nav-login').style.display = 'none';
     document.getElementById('nav-register').style.display = 'none';
@@ -20,7 +20,7 @@ function showPage(pageId) {
 }
 
 // 초기 로딩 시 JWT 확인
-const token = localStorage.getItem('token');
+const token = localStorage.getItem('accessToken');
 if (token) showPage('search');
 else showPage('auth');
 
@@ -30,9 +30,40 @@ document.getElementById('nav-register').addEventListener('click', () => showPage
 document.getElementById('nav-search').addEventListener('click', () => showPage('search'));
 document.getElementById('nav-playlist').addEventListener('click', () => showPage('playlist'));
 document.getElementById('nav-logout').addEventListener('click', () => {
-  localStorage.removeItem('token');
+  localStorage.removeItem('accessToken');
   showPage('auth');
 });
+
+// 로그인 API 호출
+document.getElementById('login-section').addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const id = document.getElementById('login-id').value;
+  const password = document.getElementById('login-password').value;
+  console.log(id, password);
+  try {
+    const response = await fetch('http://localhost:3000/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id, password })
+    });
+    
+    const data = await response.json();
+    
+    if(!response.ok){
+      alert(data.error || '로그인 실패');
+      return;
+    }
+  
+    localStorage.setItem('accessToken', data.token);
+    alert('로그인 성공');
+    showPage('search');
+
+  } catch (error) {
+    console.error('로그인 에러:', error);
+    alert('서버와 연결할 수 없습니다. 잠시 후 다시 시도해주세요.');
+    return;
+  }
+})
 
 // 검색 입력 이벤트
 document.getElementById('search-input').addEventListener('keypress', async (e) => {
